40a65198285f373144f110d225d895d7b72f2fef
Nitish Naik <nitishnaik2022@gmail.com>
2024-03-24 20:48:28 +0530

    First commit

---
+++ Card_Effect/app.js
@@ -0,0 +1,17 @@
+// import Swiper from 'swiper';
+<script type="module">
+  import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+  const swiper = new Swiper(...)
+</script>
+var swiper = new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+    speed: 500,
+    loop: true,
+    rotate: true,
+    mousewheel: {
+    invert: false,
+  },
+});
